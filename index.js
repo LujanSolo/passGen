e5f36e552536ac07b349856e5bb3c22752e4a2bb
LujanSolo <lujansolo@proton.me>
2023-04-11 02:35:03 -0700

    restructuring javascript

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-// bring in both empty h3's to display the passwords
-let boxOneEl = document.getElementById('boxOne-el');
-let boxTwoEl = document.getElementById('boxTwo-el');
-
-
+// the array of characters to build the password
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-console.log(characters.length);
 
-function generatePass(){
-  let passOne = Math.floor(Math.random() * characters.length);
-  console.log(passOne)
-}
+// set the values of "boxOne-el" and "boxTwo-el" to variables in order to set the textContent
+const boxOneEl = document.getElementById("boxOne-el");
+const boxTwoEl = document.getElementById("boxTwo-el");
 
-// each password is 15 characters long
-// get a random 15 elements from the characters array
-// create two random passwords when the function is called and print them to their respective boxes
+// pull between 10-15 random characters from the array with Math.random
+// add each result to the textContent of the appropriate box, as there are 2 password results at a time
