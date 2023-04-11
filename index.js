8da6c47753ea77465503b384923d4f8d882c9626
LujanSolo <lujansolo@proton.me>
2023-04-11 02:37:25 -0700

    defined password length variable to 15

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+// set the amount of characters in the password
+let passwordLength = 15;
+
 // set the values of "boxOne-el" and "boxTwo-el" to variables in order to set the textContent
 const boxOneEl = document.getElementById("boxOne-el");
 const boxTwoEl = document.getElementById("boxTwo-el");
