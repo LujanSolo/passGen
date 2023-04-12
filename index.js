87aa5f1b2385cc92c96a937d08878bcfde6e3ffc
LujanSolo <lujansolo@proton.me>
2023-04-12 00:24:59 -0700

    built functionality for user selected password length. created comments throughout javascript. tweaked naming conventions in select element on html

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
-// the array of characters to build the password
+//* the array of characters to build the password
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-// set the amount of characters in the password
-let passwordLength = 15;
-
-// set the values of "boxOne-el" and "boxTwo-el" to variables in order to set the textContent
+//* set the values of "boxOne-el" and "boxTwo-el" to variables in order to set the textContent
 const boxOneEl = document.getElementById("boxOne-el");
 const boxTwoEl = document.getElementById("boxTwo-el");
 
-// pull between 10-15 random characters from the array with Math.random
-function getRandomIndex() {
-  // let randomCharacter = characters[(Math.floor(Math.random() * characters.length))];
-  // return randomCharacter;
+//* set password length to call function to get user's selected value
+let passwordLength = updateOption(); 
 
-  let randomIndex = Math.floor(Math.random() * characters.length);
-  return characters[randomIndex];
+//* function to return the value of the user's password length selection
+function updateOption() {
+  const userChoiceEl = document.getElementById("user-choice");
+  const passwordLength = userChoiceEl.value;
+  return passwordLength;
 }
 
-// add each result to the textContent of the appropriate box, as there are 2 password results at a time
+//* function to pull a random index from the characters array
+function getRandomIndex() {
+  let randomIndex = Math.floor(Math.random() * characters.length);
+  return characters[randomIndex];
+
+  //! another way to write it:
+  // let randomCharacter = characters[(Math.floor(Math.random() * characters.length))];
+  // return randomCharacter;
+}
+
+//* function to add each result to the textContent of the appropriate box, as there are 2 password results at a time
 function generatePass(){
-  boxOneEl.textContent = "";
+  passwordLength = updateOption(); // set password length based on user's choice
+  boxOneEl.textContent = "";  // clear out both boxes so text doesn't overlap on each button click
   boxTwoEl.textContent = "";
+
+  // for loop continuously adds values from chracters array to box, based on password length
   for(let i=0; i < passwordLength; i++){
     boxOneEl.textContent += getRandomIndex();
   };
 
+  // method within generatePass() that automatically generates a second password with one click
   function genPassTwo() {
+
+    // for loop continuously adds values from chracters array to box, based on password length
     for(let i=0; i < passwordLength; i++){
       boxTwoEl.textContent += getRandomIndex();
   }};
-
+  // call the method
   genPassTwo();
 }
+
+//* function to copy password one to clipboard
 function copyPasswordOne() {
   let copyText = boxOneEl.innerHTML;
-  
+
   navigator.clipboard.writeText(copyText).then(() => {
     alert('Copied first password to clipboard');
   })
