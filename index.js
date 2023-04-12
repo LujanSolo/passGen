764543b312b4e464eb7ed8911ac9e99e9b75d199
LujanSolo <lujansolo@proton.me>
2023-04-12 00:59:30 -0700

    included a regEx in order to build a filtered array that returns only alphabetical characters from the characters array

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-//* the array of characters to build the password
+  //* the array of characters to build the password
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
 
-//* set the values of "boxOne-el" and "boxTwo-el" to variables in order to set the textContent
+  //* a new array is set to a constant, built by filtering through the characters array and implementing a regular expression (regEx) to return only alphabetical characters
+const lettersOnlyArray = characters.filter(char => /^[a-z]+$/i.test(char));
+console.log(lettersOnlyArray);
+        //! regEx details: ^: Matches the start of the string.
+        //! [a-z]+: Matches one or more characters that are within the range of "a" through "z" (lowercase only).
+        //! $: Matches the end of the string.
+        //! /i: Makes the regex case-insensitive, so it will match uppercase and lowercase letters.
+
+
+
+  //* set the values of "boxOne-el" and "boxTwo-el" to variables in order to set the textContent
 const boxOneEl = document.getElementById("boxOne-el");
 const boxTwoEl = document.getElementById("boxTwo-el");
 
 
-//* set password length to call function to get user's selected value
+  //* set password length to call function to get user's selected value
 let passwordLength = updateOption(); 
 
 
-//* function to return the value of the user's password length selection
+  //* function to return the value of the user's password length selection
 function updateOption() {
   const userChoiceEl = document.getElementById("user-choice");
   const passwordLength = userChoiceEl.value;
@@ -20,7 +30,7 @@ function updateOption() {
 };
 
 
-//* function to pull a random index from the characters array
+  //* function to pull a random index from the characters array
 function getRandomIndex() {
   let randomIndex = Math.floor(Math.random() * characters.length);
   return characters[randomIndex];
