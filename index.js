cd13b81e99fe6b34b55201e8c33ce89e64679994
LujanSolo <lujansolo@proton.me>
2023-04-12 02:22:10 -0700

    cleaned up comments and added stylings to labels. pseudocoding for js and div cleanup on html

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-  //* the array of characters to build the password
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+//* the array of characters to build the password
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+  "/"];
 
 
-  //* a new array is set to a constant, built by filtering through the characters array and implementing a regular expression (regEx) to return only alphabetical characters
+//* a new array is set to a constant, built by filtering through the characters array and implementing a regular expression (regEx) to return only alphabetical characters
 const lettersOnlyArray = characters.filter(char => /^[a-z]+$/i.test(char));
-console.log(lettersOnlyArray);
-        //! regEx details: ^: Matches the start of the string.
-        //! [a-z]+: Matches one or more characters that are within the range of "a" through "z" (lowercase only).
-        //! $: Matches the end of the string.
-        //! /i: Makes the regex case-insensitive, so it will match uppercase and lowercase letters.
+
+//! regEx details: ^: Matches the start of the string.
+//! [a-z]+: Matches one or more characters that are within the range of "a" through "z" (lowercase only).
+//! $: Matches the end of the string.
+//! /i: Makes the regex case-insensitive, so it will match uppercase and lowercase letters.
 
 
-
-  //* set the values of "boxOne-el" and "boxTwo-el" to variables in order to set the textContent
+//* set the values of "boxOne-el" and "boxTwo-el" to variables in order to set the textContent
 const boxOneEl = document.getElementById("boxOne-el");
 const boxTwoEl = document.getElementById("boxTwo-el");
 
 
-  //* set password length to call function to get user's selected value
-let passwordLength = updateOption(); 
+//* set password length to call function to get user's selected value
+let passwordLength = updateLength();
 
 
-  //* function to return the value of the user's password length selection
-function updateOption() {
+//* function to return the value of the user's password length selection
+function updateLength() {
   const userChoiceEl = document.getElementById("user-choice");
   const passwordLength = userChoiceEl.value;
   return passwordLength;
 };
 
+//todo add if-statement to generatePass() to say "IF THE CHECKBOX IS CHECKED, USE THE characters ARRAY, else USE THE lettersOnlyArray"
 
-  //* function to pull a random index from the characters array
+//* function to pull a random index from the characters array
 function getRandomIndex() {
+  
   let randomIndex = Math.floor(Math.random() * characters.length);
   return characters[randomIndex];
 
@@ -42,13 +43,13 @@ function getRandomIndex() {
 
 
 //* function to add each result to the textContent of the appropriate box, as there are 2 password results at a time
-function generatePass(){
-  passwordLength = updateOption(); // set password length based on user's choice
+function generatePass() {
+  passwordLength = updateLength(); // set password length based on user's choice
   boxOneEl.textContent = "";  // clear out both boxes so text doesn't overlap on each button click
   boxTwoEl.textContent = "";
 
   // for loop continuously adds values from chracters array to box, based on password length
-  for(let i=0; i < passwordLength; i++){
+  for (let i = 0; i < passwordLength; i++) {
     boxOneEl.textContent += getRandomIndex();
   };
 
@@ -56,9 +57,10 @@ function generatePass(){
   function genPassTwo() {
 
     // for loop continuously adds values from chracters array to box, based on password length
-    for(let i=0; i < passwordLength; i++){
+    for (let i = 0; i < passwordLength; i++) {
       boxTwoEl.textContent += getRandomIndex();
-  }};
+    }
+  };
   // call the method
   genPassTwo();
 };
